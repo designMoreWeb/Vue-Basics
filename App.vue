6a4fe429ecac5f5569719d264c169e5818c3d53e
designMoreWeb <divystolia@gmail.com>
2017-08-23 16:43:47 -0400

    basic
    Basics of vue js

---
+++ App.vue
@@ -0,0 +1,25 @@
+<!--Templates-->
+<template>
+  <div id="app"> 
+    <input type="text" v-on:input="changeTitle">
+    <p>{{ message }}</p>
+</div>
+</template>
+
+<!--Javascript-->
+<script src="https://unpkg.com/vue/dist/vue.js">
+ new Vue({
+     el: '#app',
+     data:{
+         message:'Vue JS is my fav framework' 
+     },
+     methods:{
+         changeTitle: function(){
+            this.message = event.target.value;         
+         }
+     }
+ });
+
+</script>
+
+<!--CSS-->
